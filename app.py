1ef8752f2b91cb87f71f0883874b531c0edf3b82
RCD2020 <dobertravis@icloud.com>
2023-10-07 17:05:50 -0600

    added ability to upload images

--- app.py
+++ app.py
@@ -1,10 +1,50 @@
-from flask import Flask
+import os
+from flask import (
+    flash, Flask, redirect, render_template, request, url_for
+)
+from werkzeug.utils import secure_filename
 
+# Restraints and paths for image uploads
+UPLOAD_FOLDER = 'static/images/source'
+ALLOWED_EXTENSIONS = {'png'}
+
+# initialize the app and configure the upload folder
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
+
+# check if file is correct type
+def allowed_file(filename):
+    return '.' in filename \
+        and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
 def landing():
-    return 'success'
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('Must be a .png file.')
+            return render_template('index.html')
+        
+        # get POSTed file
+        file = request.files['file']
+
+        # check if anything was POSTed
+        if file.filename == '':
+            flash('No selected file.')
+            return render_template('index.html')
+        
+        # check that there is a file and is allowed type
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(
+                os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            )
+            return f'<img src="{app.config["UPLOAD_FOLDER"]}/{filename}">'
+        
+
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
