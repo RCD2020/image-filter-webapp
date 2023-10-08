4e3958c2808130f6891f82d3def9e9e41b8dd1c7
RCD2020 <dobertravis@icloud.com>
2023-10-07 19:00:49 -0600

    reformatted filters

--- app.py
+++ app.py
@@ -27,37 +27,53 @@ def allowed_file(filename):
 
 @app.route('/', methods=['GET', 'POST'])
 def landing():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('Must be a .png file.')
+            return redirect('/')
+        
+        # get POSTed file
+        file = request.files['file']
+
+        # check if anything was POSTed
+        if file.filename == '':
+            flash('No selected file.')
+            return redirect('/')
+        
+        # check that there is a file and is allowed type
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(
+                os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            )
+            return redirect(url_for(
+                'filter_page',
+                filename=filename
+            ))
+        else:
+            flash('Invalid file type.')
+            return redirect('/')
+
     return render_template('index.html')
 
 
-@app.route('/filter', methods=['POST'])
-def filter_page():
-    # check if the post request has the file part
-    if 'file' not in request.files:
-        flash('Must be a .png file.')
-        return redirect('/')
+@app.route('/filter/<filename>')
+def filter_page(filename):
     
-    # get POSTed file
-    file = request.files['file']
 
-    # check if anything was POSTed
-    if file.filename == '':
-        flash('No selected file.')
-        return redirect('/')
+    return render_template(
+        'filter.html', path=filename
+    )
     
-    # check that there is a file and is allowed type
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(
-            os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        )
-        return render_template(
-            'filter.html',
-            path=f'{app.config["UPLOAD_FOLDER"]}/{filename}'
-        )
-    else:
-        flash('Invalid file type.')
-        return redirect('/')
+
+@app.route('/filtered', methods=['POST'])
+def filtered_page():
+    filter_text = request.form.get('filter-text')
+
+    if not filter_text:
+        flash('No filter provided')
+        redirect()
 
 
 if __name__ == '__main__':
