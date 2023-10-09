2e8bb5a51bddee2b019ec9656e5d8b73e1f9dd50
RCD2020 <dobertravis@icloud.com>
2023-10-09 13:56:51 -0600

    started tokenizer

--- imgfilter.py
+++ imgfilter.py
@@ -5,7 +5,7 @@ class InputStream:
     """This is the Input Stream. This will give us operations to read 
     characters from our input."""
 
-    def __init__(self, text):
+    def __init__(self, text: str):
         self.text = text
         self.pos = 0
         self.line = 1
@@ -52,6 +52,63 @@ class InputStream:
     def __repr__(self) -> str:
         return self.__str__()
     
+
+class Token:
+    def __init__(self, tType: str, value):
+        self.type = tType
+        self.value = value
+
+
+    def __str__(self):
+        return f'Token(type: {self.type}, value: {self.value})'
+
+
+class Tokenizer:
+    keywords = " if then else lambda true false "
+    def __init__(self, inputStream : InputStream):
+        self.stream = inputStream
+        self.current = None
+
+
+    def isKeyword(self, x):
+        return self.keywords.find(' ' + x + ' ') >= 0
+    
+
+    def isDigit(self, ch):
+        return re.search('[0-9]', ch)
+    
+
+    def isIdStart(self, ch):
+        return re.search('[A-Za-z_]', ch)
+    
+    
+    def isId(self, ch):
+        return self.isIdStart(ch) or re.search("[0-9]", ch)
+    
+
+    def isOpChar(self, ch):
+        return '+-*/%=&|<>!'.find(ch) >= 0
+    
+
+    def isPunc(self, ch):
+        return ',;(){}[]'.find(ch) >= 0
+    
+
+    def isWhitespace(self, ch):
+        return ' \t\n'.find(ch) >= 0
+    
+
+    def readWhile(self, predicate):
+        text = ''
+        while not self.stream.eof() and predicate(self.stream.peek()):
+            text += self.stream.next()
+        return text
+    
+
+    def readNumber(self):
+        pass
+
+
 
 class ImgFilter:
     def __init__(self, path):
