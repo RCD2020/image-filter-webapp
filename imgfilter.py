030be620adb9203679e80256d27b3e280461c018
RCD2020 <dobertravis@icloud.com>
2023-10-09 12:00:56 -0600

    added input stream

--- imgfilter.py
+++ imgfilter.py
@@ -1,7 +1,73 @@
 from PIL import Image
+import re
+
+class InputStream:
+    """This is the Input Stream. This will give us operations to read 
+    characters from our input."""
+
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.line = 1
+        self.col = 0
+    
+    
+    def peek(self) -> str:
+        "Returns the next value but without removing it from the stream"
+
+        try:
+            return self.text[self.pos]
+        except:
+            return ''
+        
+
+    def next(self) -> str:
+        "Returns the next value and discards it from the stream"
+
+        ch = self.peek()
+        self.pos += 1
+        if ch == '\n':
+            self.line += 1
+            self.col = 0
+        else:
+            self.col += 1
+
+        return ch
+    
+
+    def eof(self) -> bool:
+        "Returns true if at the end of the file"
+
+        return self.peek() == ''
+    
+
+    def __str__(self) -> str:
+        return f'InputStream(pos: {self.pos}, line: {self.line}, col: {self.col})'
+    
+
+    def __repr__(self) -> str:
+        return self.__str__()
+    
 
 class ImgFilter:
     def __init__(self, path):
         self.img = Image.open(path)
+        self.vars = {}
 
-    
+
+    # def readCode(text):
+    #     lines = text.split(';')
+    #     current = ''
+    #     for line in lines:
+    #         for char in line:
+    #             if re.search("[A-Za-z0-9_]", char):
+    #                 current += char
+    #                 print(char, 'is not /[A-Za-z0-9_]/g', current)
+    #             else:
+    #                 current = ''
+    #                 print(char, 'is /[A-Za-z0-9_]/g', current)
+            
+
+if __name__ == '__main__':
+    pass
+    # ImgFilter.readCode('test = 54 + 6')
