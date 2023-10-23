d8e1ed117e6633c0e0726ae820a8058837018102
RCD2020 <dobertravis@icloud.com>
2023-10-23 17:47:13 -0600

    added sepia tone example

--- static/js/filtered.js
+++ static/js/filtered.js
@@ -17,6 +17,43 @@ for (x = 0; x < width; x = x + 1) {
     };
 };`;
 
+const SEPIA = `# loop through pixels
+for (x = 0; x < width; x = x + 1)
+{
+    for (y = 0; y < height; y = y + 1)
+    {
+        # loads variables r, g, b into memory
+        loadColor(x, y);
+
+        # this algorithm applies math to make our image appear sepia toned
+        sr = 0.393 * r + 0.769 * g + 0.189 * b + 0.5;
+        sg = 0.349 * r + 0.686 * g + 0.168 * b + 0.5;
+        sb = 0.272 * r + 0.534 * g + 0.131 * b + 0.5;
+
+        # and now we must check that none of our pixels went over 255
+        if (sr > 255)
+        {
+            sr = 255;
+        };
+        if (sg > 255)
+        {
+            sg = 255;
+        };
+        if (sb > 255)
+        {
+            sb = 255;
+        };
+
+        # save it to image
+        pixels[x, y] = rgb(sr, sg, sb);
+    };
+};`;
+
 function grayscale() {
     editor.getDoc().setValue(GRAYSCALE);
+}
+
+
+function sepia() {
+    editor.getDoc().setValue(SEPIA);
 }
